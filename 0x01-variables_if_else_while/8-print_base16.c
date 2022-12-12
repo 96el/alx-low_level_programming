e8db72c0efa4e64f286af555a42b5ec036eb02ad
96el <abelmathiosgebru@gmail.com>
2022-12-12 00:50:10 -0800

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  * Return: Always 0 (success)
  */
 int main(void)
@@ -15,8 +15,8 @@ int main(void)
 			putchar (num + '0');
 		} else
 		{
-			putchar(c)
-				c++;
+			putchar(c);
+			c++;
 		}
 	}
 	putchar('\n');
