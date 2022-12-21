4a639afbc9b3dcf75c6fda91797080db5ed7c176
96el <abelmathiosgebru@gmail.com>
2022-12-20 16:48:40 -0800

    task 6

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@ int _sterlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-void puts2_half(char *str);
+void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
