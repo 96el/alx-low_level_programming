1196aa4371d9f384a8ed152495be2f08045355f2
96el <abelmathiosgebru@gmail.com>
2023-01-31 01:15:01 -0800

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,7 @@
  * @head: pointer to the head of the list
  * @index: index of the node required
  * Return: the address of the node
- *
+ */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
